9b27bb9d909b18479fdbbbc2a2d0b99b6a07273c
Semyon Usachev <51188729+Kerlenton@users.noreply.github.com>
2020-06-11 08:05:08 +0400

    Update caesar.h

--- caesar/caesar.h
+++ caesar/caesar.h
@@ -1,14 +1,20 @@
 #pragma once
 #include <stddef.h>
 
-#define MAX_LENGTH 127
-#define LEN_ALPHA 26
-#define END_OF_STRING '\0'
-#define ENCRYPT_MODE 1
+#define ENCRYPT_MODE  1
 #define DECRYPT_MODE -1
 
+#define END_OF_STRING  0
+#define END_OF_NUMBER -128
+
+#define LEN_ALPHA 26
+#define MAX_LENGTH 127
+
 static char _char_caesar(const char key, const char c);
+
 static void strcpy(char *s, char *t);
-size_t strlen(const char *str);
+static size_t strlen(const char *str);
+
 extern int set_alpha_caesar(const char *alpha);
 extern int caesar(char *to, char *from, int key, const int mode);
+extern void print_array(char *arr, size_t length);
