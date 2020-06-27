fb0fdd77e680e4e72efda3af676a34d37616dfa2
Semyon Usachev <51188729+Kerlenton@users.noreply.github.com>
2020-06-27 16:01:45 +0400

    Update rot13.h

--- rot13/rot13.h
+++ rot13/rot13.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <stdio.h>
-
 #define ENCRYPT_MODE  1
 #define DECRYPT_MODE -1
 
@@ -11,6 +9,8 @@
 #define LEN_ALPHA 26
 #define MAX_LENGTH 127
 
-extern int set_alpha_rot13(const char *alpha);
+#define NULL ((void*) 0)
+
+extern int set_alpha_caesar(const char *alpha);
 extern void print_array(char *arr);
 extern void rot13(char *to, char *from);
